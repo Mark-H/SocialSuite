6cedcfbb6e95635a81aa7470f1ec11c8fc868343
Mark Hamstra <hello@markhamstra.com>
2012-04-30 00:32:36 +0200

    Doc updates

--- core/components/socialsuite/elements/snippets/getTwitterProfile.snippet.php
+++ core/components/socialsuite/elements/snippets/getTwitterProfile.snippet.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * SocialSuite
+ * SocialSuite - getTwitterProfile
  *
  * Copyright 2011 by Mark Hamstra <hello@markhamstra.com>
  *
-* This file is part of SocialSuite.
+ * This file is part of SocialSuite.
  *
  * SocialSuite is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
